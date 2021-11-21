0e2e4a210d95ff8e3e0543d10a82bcf9ff206f03
songk1992-home <zuppiy@naver.com>
2021-11-21 11:01:59 +0900

    Logging of Authentication Failure Events

--- src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
+++ src/main/java/guru/sfg/brewery/security/listeners/AuthenticationFailureListener.java
@@ -1,34 +1,33 @@
 package guru.sfg.brewery.security.listeners;
 
-import guru.sfg.brewery.domain.security.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class AuthenticationSuccessListener {
+public class AuthenticationFailureListener {
 
     @EventListener
-    public void listen(AuthenticationSuccessEvent event) {
-        log.debug("User Logged In Okay");
-
+    public void listen(AuthenticationFailureBadCredentialsEvent event) {
+        log.debug("login failure");
         if (event.getSource() instanceof UsernamePasswordAuthenticationToken) {
             UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) event.getSource();
 
-            if (token.getPrincipal() instanceof User) {
-                User user = (User) token.getPrincipal();
-
-                log.debug("User name logged in " + user.getUsername());
+            if (token.getPrincipal() instanceof String) {
+                log.debug("attempted username : " + token.getPrincipal());
             }
 
             if (token.getDetails() instanceof WebAuthenticationDetails) {
                 WebAuthenticationDetails details = (WebAuthenticationDetails) token.getDetails();
                 log.debug("Source IP : " + details.getRemoteAddress());
             }
+
         }
     }
+
+
 }
